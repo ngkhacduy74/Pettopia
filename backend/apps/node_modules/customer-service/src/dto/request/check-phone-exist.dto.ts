2a576c0cdfcb6ae3802665efc94bbb2d8f075f13
ngkhacduy74 <duycaduoc0704@gmail.com>
2025-10-06 16:34:13 +0700

    Save current changes before merge

--- backend/apps/node_modules/customer-service/src/dto/request/check-phone-exist.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsPhoneNumber } from 'class-validator';
-
-export class CheckPhoneExistDto {
-  @IsPhoneNumber('VN') 
-  phone_number: string;
-}
--- backend/apps/node_modules/customer-service/src/dto/request/get-user-by-id.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-export class GetUserByIdDto {
-    @IsString()
-    @IsNotEmpty()
-    id: string;
-  }
