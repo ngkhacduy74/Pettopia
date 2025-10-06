dd287adea0dee294010c765c645149b01357f1e8
ngkhacduy74 <duycaduoc0704@gmail.com>
2025-10-06 16:08:53 +0700

    add missing config files (.gitignore, dockerignore, tsconfig...)

--- backend/apps/auth-service/src/app.service.ts
+++ backend/apps/auth-service/src/app.service.ts
@@ -1,38 +1,38 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
-import { LoginDto } from './dtos/login.dto';
-import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { RegisterDto } from './dtos/register.dto';
-import { lastValueFrom } from 'rxjs';
-import * as bcrypt from 'bcrypt';
-import { JwtService } from '@nestjs/jwt';
-import * as uuid from 'uuid';
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { LoginDto } from "./dtos/login.dto";
+import { ClientProxy, RpcException } from "@nestjs/microservices";
+import { RegisterDto } from "./dtos/register.dto";
+import { lastValueFrom } from "rxjs";
+import * as bcrypt from "bcrypt";
+import { JwtService } from "@nestjs/jwt";
+import * as uuid from "uuid";
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('CUSTOMER_SERVICE') private customerClient: ClientProxy,
-    private readonly jwtService: JwtService,
+    @Inject("CUSTOMER_SERVICE") private customerClient: ClientProxy,
+    private readonly jwtService: JwtService
   ) {}
   async login(data: LoginDto): Promise<any> {
-    console.log('data customer service', data);
+    console.log("data customer service", data);
     try {
       const exist_user = await lastValueFrom(
         this.customerClient.send(
-          { cmd: 'getUserByUsername' },
-          { username: data.username },
-        ),
+          { cmd: "getUserByUsername" },
+          { username: data.username }
+        )
       );
 
       if (!exist_user) {
-        throw new NotFoundException('Không tìm thấy user');
+        throw new NotFoundException("Không tìm thấy user");
       }
       const isMatch = await bcrypt.compare(data.password, exist_user.password);
       if (!isMatch) {
-        throw new RpcException('Mật khẩu không đúng');
+        throw new RpcException("Mật khẩu không đúng");
       }
       const { password, ...result } = exist_user;
       const token = this.jwtService.sign(result);
       return { status: true, token: token };
-    } catch (err) {
+    } catch (err: any) {
       throw new Error(err);
     }
   }
@@ -41,26 +41,26 @@ export class AppService {
     try {
       const exist_phone = await lastValueFrom(
         this.customerClient.send(
-          { cmd: 'checkPhoneExist' },
-          { phone_number: data.phone_number },
-        ),
+          { cmd: "checkPhoneExist" },
+          { phone_number: data.phone_number }
+        )
       );
 
       const exist_email = await lastValueFrom(
         this.customerClient.send(
-          { cmd: 'getUserByEmail' },
-          { email_address: data.email_address },
-        ),
+          { cmd: "getUserByEmail" },
+          { email_address: data.email_address }
+        )
       );
 
       if (exist_email.status == false) {
         throw new RpcException(
-          'Email đã được đăng ký bằng 1 tài khoản khác. Vui lòng sử dụng email khác',
+          "Email đã được đăng ký bằng 1 tài khoản khác. Vui lòng sử dụng email khác"
         );
       }
 
       if (exist_phone.status == false) {
-        throw new RpcException('Đăng ký thất bại! Số điện thoại đã tồn tại');
+        throw new RpcException("Đăng ký thất bại! Số điện thoại đã tồn tại");
       }
 
       const salt = await bcrypt.genSalt(10);
@@ -84,16 +84,16 @@ export class AppService {
         },
         is_active: true,
       };
-      console.log('newUser', newUser);
+      console.log("newUser", newUser);
       const savedUser = await lastValueFrom(
-        this.customerClient.send({ cmd: 'createUser' }, newUser),
+        this.customerClient.send({ cmd: "createUser" }, newUser)
       );
 
       return savedUser;
-    } catch (err) {
-      console.error('Register error:', err);
+    } catch (err: any) {
+      console.error("Register error:", err);
       throw new RpcException(
-        err?.message || 'Lỗi không xác định từ auth-service',
+        err?.message || "Lỗi không xác định từ auth-service"
       );
     }
   }
