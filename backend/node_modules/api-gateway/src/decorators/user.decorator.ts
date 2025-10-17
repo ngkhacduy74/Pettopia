a8a30057bbb719aac2c8ee184d03b3701be33f14
quybao18 <quybao18@gmail.com>
2025-10-17 22:48:58 +0700

    update verifile

--- backend/node_modules/api-gateway/src/decorators/user.decorator.ts
+++ backend/node_modules/api-gateway/src/decorators/user.decorator.ts
@@ -4,6 +4,7 @@ export const UserToken = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const user = request.user;
+    console.log('okaoias', user);
     if (!data) return user;
     return user ? user[data] : undefined;
   },
