08f8f0ec935a5b4c88f634f58924addd6b6207b0
quybao18 <quybao18@gmail.com>
2025-11-13 18:01:03 +0700

    cứu nhánh Bảo

--- backend/node_modules/auth-service/src/dtos/send-mail.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-export class SendEmailOtpDto {
-  @IsEmail({}, { message: 'Email không hợp lệ.' })
-  @IsNotEmpty({ message: 'Email không được để trống.' })
-  @IsString({ message: 'Email phải là chuỗi ký tự.' })
-  email: string;
-}
--- backend/node_modules/petcare-service/src/common/id_identify.common.ts
+++
@@ -1,8 +0,0 @@
-import { customAlphabet } from 'nanoid';
-
-export function generatePetId(): string {
-  const alphabet =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const nanoid = customAlphabet(alphabet, 7);
-  return nanoid();
-}
